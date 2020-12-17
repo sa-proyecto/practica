bfda90b467e95ebb2290225f3188a552fef8c703
Guillermo Medinilla <medinilla.guillermo@gmail.com>
2020-12-16 18:29:53 -0600

    agregar recursivo puppet-transfer.sh

--- puppet-transfer.sh
+++ puppet-transfer.sh
@@ -1,2 +1,2 @@
 scp -r /var/lib/jenkins/workspace/pipeline/dist/ medinillag@agent.medinillag.com:~/
-ssh medinillag@agent.medinillag.com "sudo cp ~/dist/. /var/www/html"
+ssh medinillag@agent.medinillag.com "sudo cp -r ~/dist/. /var/www/html"
