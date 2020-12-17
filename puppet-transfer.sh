f5cb72c2577c850439a4b21663012ed783df410d
Guillermo Medinilla <medinilla.guillermo@gmail.com>
2020-12-16 18:33:25 -0600

    update puppet transfer

--- puppet-transfer.sh
+++ puppet-transfer.sh
@@ -1,2 +1,2 @@
-scp -r /var/lib/jenkins/workspace/pipeline/dist/ medinillag@agent.medinillag.com:~/
-ssh medinillag@agent.medinillag.com "sudo cp -r ~/dist/. /var/www/html"
+scp -r /var/lib/jenkins/workspace/pipeline/dist/practica-lab-sa medinillag@agent.medinillag.com:~/
+ssh medinillag@agent.medinillag.com "sudo cp -r ~/practica-lab-sa/. /var/www/html"
